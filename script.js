af9ee3854553bbd303b6381ff88dcc642f8acd8d
Junaid Pathan <jpathan@umass.edu>
2025-01-21 16:48:16 +0300

    Randomized Numbers

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded",click)
-function click(){ 
-    document.querySelectorAll('button').forEach((button)=>{
-        button.onclick = function(){ 
-            if (button.dataset.operation === 'add'){
+function click(){  //to see which button u clicked
+    document.querySelectorAll('button').forEach((button)=>{ 
+        button.onclick = function(){  //only when clicked this function runs. 
+            if (button.dataset.operation === 'add'){ //data-operation is the field in the button
                 add()
             }
             else{
@@ -13,11 +13,13 @@ function click(){
 }
 
 function add(){ 
-    document.querySelector('#firstpage').style.display = 'none'
+    document.querySelector('#firstpage').style.display = 'none' //firstpage div is hidden and hide div is shown
     document.querySelector('#hide').style.display='block'
     let num1 = Math.ceil(Math.random()*10)
     let num2 = Math.ceil(Math.random()*10)
     console.log(num1,num2)
+    document.querySelector('#num1').innerHTML=num1
+    document.querySelector('#num2').innerHTML=num2
 }
 function multiply(){ 
 
